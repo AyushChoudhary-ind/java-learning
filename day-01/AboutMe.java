6fcf606e682f778968301e6901d39a1388fb9a86
Ayush Choudhary <ayush.choudhary.ind@gmail.com>
2026-08-12 23:44:56 +0530

    Day 1: first Java programs

---
+++ day-01/AboutMe.java
@@ -0,0 +1,16 @@
+class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("========================\r\n" + //
+                        "       ABOUT ME\r\n" + //
+                        "========================\r\n" + //
+                        "\r\n" + //
+                        "Name: Ayush\r\n" + //
+                        "Age: 24\r\n" + //
+                        "Country: India\r\n" + //
+                        "Goal: Java Developer\r\n" + //
+                        "Learning: Java\r\n" + //
+                        "Experience: Beginner\r\n" + //
+                        "Day: 1\r\n" + //
+                        "Dream: Become a successful Java developer");
+    }
+}
---
+++ day-01/MyGoals.java
@@ -0,0 +1,11 @@
+class MyGoals {
+    public static void main(String[] args) {
+        System.out.println("My Goals\r\n" + //
+                        "---------\r\n" + //
+                        "1. Learn Java\r\n" + //
+                        "2. Learn Git\r\n" + //
+                        "3. Learn GitHub\r\n" + //
+                        "4. Build projects\r\n" + //
+                        "5. Become a developer");
+    }
+}
